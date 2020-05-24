90bbedffbefb5334b37181c88b20fafb7620d1ce
bamvoo <aguilord99@gmail.com>
2020-05-25 01:29:49 +0200

    l&r 5.5

--- controllers/AccessController.php
+++ controllers/AccessController.php
@@ -15,7 +15,8 @@ $password_l = filter_input(INPUT_POST, 'passw_user_login');
 
 
 if ($usuari_r) {
-    $query = "INSERT INTO users (name,password) VALUES ('" . $usuari_r . "','" . $password_r . "');";
+    $query = "INSERT INTO users (name, password, level, hp, class, state, attack, floor, power) VALUES 
+('" . $usuari_r . "','" . $password_r . "','" . $level . "','" . $hp . "','" . $class . "','" . $class . "','" . $class . "');";
     $ok = $db->executeQuery($query);
     $usuari = $usuari_r;
 } else {
@@ -25,13 +26,12 @@ if ($usuari_r) {
     $usuari = $usuari_l;
 }
 if ($ok) {
-    $query = "SELECT name, level, hp, class, state, attack, floor, power points FROM users WHERE name = '" . $usuari . "';";
+    $query = "SELECT name, password, level, hp, class, state, attack, floor, power FROM users WHERE name = '" . $usuari . "';";
     $db->executeQuery($query, $datauser);
-    setcookie('userid', $datauser[0]['id'], 0, '/', 'localhost');
     setcookie('username', $datauser[0]['name'], 0, '/', 'localhost');
     setcookie('userlevel', $datauser[0]['level'], 0, '/', 'localhost');
     setcookie('userpoints', $datauser[0]['points'], 0, '/', 'localhost');
-    $_SESSION['userid']=;
+    $_SESSION['userid']= $datauser[0]['name'];
     header('location: ../viewspackage/MainView.php');
 } else {
     header('location: ../viewspackage/BadLogin.php');
