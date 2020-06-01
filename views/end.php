d3e9a9d3b434841d9c4930b75368295ee1c97286
bamvoo <aguilord99@gmail.com>
2020-06-01 17:59:56 +0200

    functions 15.9

--- views/end.php
+++ views/end.php
@@ -10,31 +10,13 @@
     </head>
 
     <body>
-        <header c>
-        </header>
-
-        <aside id="leftside">
-
-        </aside>
-
-        <aside id="rightside">
-
-        </aside>
-
-        <section id="central">
-            <article>
+        <section  class="rpgui-content grid_body">
+            <article class="rpgui-container framed-golden medio">
                 <div>
                     <h3>HAS MUERTO QUE MALO ERES, QUIERES INTENTARLO OTRA VEZ? </h3>
                     <input type="button" name="btn1" onclick="location.href='../../ProyectoM3/index.html'"; value="Volver atrás" >
                 </div>
-
-                <?php
-
-                ?>
             </article>
         </section>
-
-        <footer>
-        </footer>
     </body>
 </html>
--- views/victory.php
+++ views/victory.php
@@ -10,31 +10,15 @@
 </head>
 
 <body>
-<header c>
-</header>
-
-<aside id="leftside">
-
-</aside>
-
-<aside id="rightside">
-
-</aside>
-
-<section id="central">
-    <article>
+<section  class="rpgui-content grid_body">
+    <article class="rpgui-container framed-golden medio">
         <div>
             <h3>HAS LLEGADO CON VIDA AL EXTERIOR! FELICIDADES!</h3>
             <p>Muy pocos han conseguidos salir vivos de esta trampa mortal, serás recordado por la eternidad.</p>
             <input type="button" name="btn1" onclick="location.href='../../ProyectoM3/index.html'"; value="Voler atrás" >
         </div>
-        <?php
-
-        ?>
     </article>
 </section>
 
-<footer>
-</footer>
 </body>
 </html>
